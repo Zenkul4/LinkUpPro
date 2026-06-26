ad9d8f3ec26e97ef93bcbc074b4c90b59e354d4d
PaulSanche <psanchezm15@gmail.com>
2026-06-26 00:16:20 -0400

    Proyecto compilando correctamente

---
+++ LinkUpPro/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LinkUpPro.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
